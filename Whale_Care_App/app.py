d8fba7b64b64e10a10ce995938e43b0f4811d836
Artemis <132653903+Artemis-IA@users.noreply.github.com>
2023-07-28 16:29:46 +0200

    Add files via upload

--- Whale_Care_App/app.py
+++ Whale_Care_App/app.py
@@ -1,77 +1,55 @@
+# app.py
 from flask import Flask, render_template, request
-from models.data import Data
-import requests
-
+from models.data_manager import DataManager
+from models.validators import Validators
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+class AppController:
+    @app.route('/')
+    def index():
+        return render_template('index.html')
 
-@app.route('/form')
-def fill_form():
-    return render_template('form.html')
+    @app.route('/form')
+    def fill_form():
+        return render_template('form.html')
 
-@app.route('/greeting', methods=['POST'])
-def form_collect():
-    if request.method == 'POST':
-        nom = request.form.get('nom')
-        prenom = request.form.get('prenom')
-        adresse = request.form.get('adresse')
-        code_postal = request.form.get('code_postal')
-        ville = request.form.get('ville')
-        email = request.form.get('email')
-        somme_recoltee = request.form.get('somme_recoltee')
+    @app.route('/greeting', methods=['POST'])
+    def form_collect():
+        if request.method == 'POST':
+            form_data = request.form
+            data_handler = DataManager()
+            validators = Validators()
+            try:
+                if validators.validate_form_data(form_data):
+                    donator = data_handler.handle_form_data(form_data)
+                    return render_template('greeting.html', donator=donator)
+                else:
+                    raise ValueError("Les données du formulaire ne sont pas valides.")
+            except ValueError as e:
+                return render_template('form.html', error_message=str(e))
+        else:
+            return render_template('index.html')  # Rediriger vers la page d'accueil en cas de requête non autorisée
 
-        # Géocodage de la ville saisie par l'utilisateur
-        geocoded_data = geocode_city(ville)
-        latitude = geocoded_data['lat'] if geocoded_data else None
-        longitude = geocoded_data['lon'] if geocoded_data else None
+    @app.route('/thank_you/<int:donator_id>')
+    def thank_you(donator_id):
+        data_handler = DataManager()
+        donator = data_handler.get_donator_by_id(donator_id)
+        if donator:
+            return render_template('thank_you.html', donator=donator)
+        else:
+            return render_template('error_page.html', error_message="Donateur non trouvé.")
 
-        data = Data()
-        try:
-            donator = data.set_form(nom, prenom, adresse, code_postal, ville, email, somme_recoltee, latitude, longitude)
-            return render_template('greeting.html', donator=donator)
-        except ValueError as e:
-            return render_template('form.html', error_message=str(e))
-    else:
-        return render_template('index.html')  # Rediriger vers la page d'accueil en cas de requête non autorisée
-    
-@app.route('/thank_you/<int:donator_id>')
-def thank_you(donator_id):
-    # Récupérer les détails du donateur depuis la base de données
-    data = Data()
-    donator = data.get_donator_by_id(donator_id)
-    if donator:
-        return render_template('thank_you.html', donator=donator)
-    else:
-        # Gérer le cas où le donateur n'a pas été trouvé dans la base de données
-        return render_template('error_page.html', error_message="Donateur non trouvé.")
+    @app.route('/view')
+    def view_collect():
+        data_handler = DataManager()
+        donators, total_sum = data_handler.get_donators_and_total_sum()
 
-# Fonction pour géocoder la ville en utilisant l'API Nominatim
-def geocode_city(city):
-    base_url = 'https://nominatim.openstreetmap.org/search'
-    params = {'q': city, 'format': 'json', 'limit': 1}
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
-    response = requests.get(base_url, params=params, headers=headers)
+        return render_template('view.html', donators=donators, total_sum=total_sum)
 
-    if response.status_code == 200:
-        data = response.json()
-        if data:
-            return data[0]
-    return None
+    @app.route('/conditions_generales')
+    def conditions_generales():
+        return render_template('conditions_generales.html')
 
-@app.route('/view')
-def view_collect():
-    data = Data()
-    donators = data.get_donators()
-    total_sum = data.get_total_sum()
-
-    return render_template('view.html', donators=donators, total_sum=total_sum)
-
-@app.route('/conditions_generales')
-def conditions_generales():
-    return render_template('conditions_generales.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
